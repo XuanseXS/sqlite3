d8605be774576bb5ef8849c7bd78068cd1e23725
xuanse <2334520090@qq.com>
2024-03-13 22:06:48 +0800

    提交代码

---
+++ mysqlite.c
@@ -0,0 +1,256 @@
+/*
+ * @Author: XuanseXS 2334520090@qq.com
+ * @Date: 2024-02-05 09:35:54
+ * @LastEditors: XuanseXS 2334520090@qq.com
+ * @LastEditTime: 2024-02-05 13:44:42
+ * @FilePath: /wicom-linux-kernel5.10-x2500lvgl-v1.4-20230927/iot/packages/apps/AI/src/mysqlite.c
+ * @Description:
+ *
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved.
+ */
+
+/* Files includes  -----------------------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include "sqlite3.h"
+#include "mysqlite.h"
+#include "sqlite_semaphore.h"
+
+/* Defines -------------------------------------------------------------------*/
+
+/* Static Variable  ----------------------------------------------------------*/
+static int sem_id = 0;
+
+/* Variable  -----------------------------------------------------------------*/
+union semun
+{
+	int val;
+	struct semid_ds *buf;
+	unsigned short *arry;
+};
+
+/* Static Fanction  ----------------------------------------------------------*/
+
+/* Fanction  -----------------------------------------------------------------*/
+sqlite3 *mysqlite_db_open(char *path)
+{
+	int ret;
+	sqlite3 *db;
+	union semun sem_union;
+
+	sem_id = semget((key_t)SEM_KEY, 4, 0666 | IPC_CREAT | IPC_EXCL);
+	if (sem_id == -1)
+	{
+		printf("semaphore get failed!\n");
+	}
+	else
+	{
+		printf("Semaphore:Set value 1 sem_id:%d\n", sem_id);
+		sem_union.val = 1;
+		semctl(sem_id, 0, SETVAL, sem_union);
+	}
+
+	ret = sqlite3_open(path, &db);
+	if (ret != SQLITE_OK)
+	{
+		printf("sqlite3_open : %s\n", sqlite3_errmsg(db));
+		return NULL;
+	}
+	else
+	{
+		return db;
+	}
+}
+
+int mysqlite_db_close(sqlite3 *db)
+{
+	sem_id = 0;
+	sqlite3_close(db);
+	return 0;
+}
+
+int mysqlite_create_table(sqlite3 *db, char *table, char *table_entry)
+{
+	char *sql_template = "CREATE TABEL IF NOT EXISTS %s (%s);";
+	char *sql = NULL;
+	int ret = 0;
+
+	sql = calloc(1, strlen(sql_template) + strlen(table) + strlen(table_entry) + 1); // 为sql动态分配内存空间
+	ret = sprintf(sql, sql_template, table, table_entry);
+	if (ret == -1)
+	{
+		printf("%s sprintf error!\n", __func__);
+		goto err;
+	}
+
+	SQL_semP(SEM_KEY, 0);
+	ret = sqlite3_exec(db, sql, 0, 0, NULL);
+	SQL_semV(SEM_KEY, 0);
+	if (ret != SQLITE_OK)
+	{
+		printf("sqlite3_exec_create_tabel : %s\n", sqlite3_errmsg(db));
+		goto err;
+	}
+
+	free(sql);
+	return 0;
+err:
+	free(sql);
+	return -1;
+}
+
+int mysqlite_insert(sqlite3 *db, char *table, char *val)
+{
+	char *sql_template = "INSERT INTO %s VALUES(%s);";
+	char *sql = NULL;
+	int ret = 0;
+
+	sql = calloc(1, strlen(sql_template) + strlen(table) + strlen(val) + 1); // 为sql动态分配内存空间
+	ret = sprintf(sql, sql_template, table, val);
+	if (ret == -1)
+	{
+		printf("%s sprintf error!\n", __func__);
+		goto err;
+	}
+
+	SQL_semP(SEM_KEY, 0);
+	ret = sqlite3_exec(db, sql, 0, 0, NULL);
+	SQL_semV(SEM_KEY, 0);
+	if (ret != SQLITE_OK)
+	{
+		printf("sqlite3_exec_insert : %s\n", sqlite3_errmsg(db));
+		goto err;
+	}
+
+	free(sql);
+	return 0;
+err:
+	free(sql);
+	return -1;
+}
+
+int mysqlite_delete(sqlite3 *db, char *table, char *content)
+{
+	char *sql_template = "DELETE FROM %s %s;";
+	char *sql = NULL;
+	int ret = 0;
+
+	sql = calloc(1, strlen(sql_template) + strlen(table) + strlen(content) + 1); // 为sql动态分配内存空间
+	ret = sprintf(sql, sql_template, table, content);
+	if (ret == -1)
+	{
+		printf("%s sprintf error!\n", __func__);
+		goto err;
+	}
+
+	SQL_semP(SEM_KEY, 0);
+	ret = sqlite3_exec(db, sql, 0, 0, NULL);
+	SQL_semV(SEM_KEY, 0);
+	if (ret != SQLITE_OK)
+	{
+		printf("sqlite3_exec_delete : %s\n", sqlite3_errmsg(db));
+		goto err;
+	}
+
+	free(sql);
+	return 0;
+err:
+	free(sql);
+	return -1;
+}
+
+int mysqlite_delete_table(sqlite3 *db, char *table)
+{
+	char *sql_template = "DROP TABLE %s %s;";
+	char *sql = NULL;
+	int ret = 0;
+
+	sql = calloc(1, strlen(sql_template) + strlen(table) + 1); // 为sql动态分配内存空间
+	ret = sprintf(sql, sql_template, table);
+	if (ret == -1)
+	{
+		printf("%s sprintf error!\n", __func__);
+		goto err;
+	}
+
+	SQL_semP(SEM_KEY, 0);
+	ret = sqlite3_exec(db, sql, 0, 0, NULL);
+	SQL_semV(SEM_KEY, 0);
+	if (ret != SQLITE_OK)
+	{
+		printf("sqlite3_exec_delete_table : %s\n", sqlite3_errmsg(db));
+		goto err;
+	}
+
+	free(sql);
+	return 0;
+err:
+	free(sql);
+	return -1;
+}
+
+int mysqlite_updata(sqlite3 *db, char *table, char *content, int sem_lock)
+{
+	char *sql_template = "UPDATE %s SET %s;";
+	char *sql = NULL;
+	int ret = 0;
+
+	sql = calloc(1, strlen(sql_template) + strlen(table) + strlen(content) + 1); // 为sql动态分配内存空间
+	ret = sprintf(sql, sql_template, table, content);
+	if (ret == -1)
+	{
+		printf("%s sprintf error!\n", __func__);
+		goto err;
+	}
+
+	if (sem_lock)
+		SQL_semP(SEM_KEY, 0);
+	ret = sqlite3_exec(db, sql, 0, 0, NULL);
+	if (sem_lock)
+		SQL_semV(SEM_KEY, 0);
+	if (ret != SQLITE_OK)
+	{
+		printf("sqlite3_exec_updata : %s\n", sqlite3_errmsg(db));
+		goto err;
+	}
+
+	free(sql);
+	return 0;
+err:
+	free(sql);
+	return -1;
+}
+
+int mysqlite_query(sqlite3 *db, char *table, char *content, char *other, query_callback_t cb, void *user_data)
+{
+	char *sql_template = "SELECT %s FROM %s %s;";
+	char *sql = NULL;
+	int ret = 0;
+
+	sql = calloc(1, strlen(sql_template) + strlen(table) + strlen(content) + strlen(other) + 1); // 为sql动态分配内存空间
+	ret = sprintf(sql, sql_template, content, table, other);
+	if (ret == -1)
+	{
+		printf("%s sprintf error!\n", __func__);
+		goto err;
+	}
+
+	SQL_semP(SEM_KEY, 0);
+	ret = sqlite3_exec(db, sql, cb, user_data, NULL);
+	SQL_semV(SEM_KEY, 0);
+	if (ret != SQLITE_OK)
+	{
+		printf("sqlite3_exec_select : %s, ret = %d\n", sqlite3_errmsg(db), ret);
+		goto err;
+	}
+
+	free(sql);
+	return 0;
+err:
+	free(sql);
+	return -1;
+}
---
+++ mysqlite.h
@@ -0,0 +1,100 @@
+/*
+ * @Author       : XuanseXS 2334520090@qq.com
+ * @Date         : 2024-02-05 09:36:28
+ * @LastEditors  : XuanseXS 2334520090@qq.com
+ * @LastEditTime : 2024-02-05 17:38:05
+ * @FilePath     : /wicom-linux-kernel5.10-x2500lvgl-v1.4-20230927/iot/packages/apps/AI/include/mysqlite.h
+ * @Description  : 
+ * 
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+ */
+
+/* Define to prevent recursive inclusion  ------------------------------------*/
+#ifndef __MYSQLITE_H
+#define __MYSQLITE_H
+
+/* Files includes  -----------------------------------------------------------*/
+#include "sqlite3.h"
+
+/* Defines -------------------------------------------------------------------*/
+#define SEM_KEY 20240205
+
+typedef int (*query_callback_t)(void *user_data, int col_count, char **col_values, char **col_name);
+
+/* Variables -----------------------------------------------------------------*/
+
+
+/* Functions ----------------------------------------------------------------*/
+
+/**
+ * @description  : 打开数据库(database)
+ * @param         {char} *path 数据库路径
+ * @return        {sqlite3 *}
+ */
+sqlite3 *mysqlite_db_open(char *path);
+
+/**
+ * @description  : 关闭数据库(database)
+ * @param         {sqlite3} *db 数据库Handle
+ * @return        {int}
+ */
+int mysqlite_db_close(sqlite3 *db);
+
+/**
+ * @description  : 在数据库中创建一个新表
+ * @param         {sqlite3} *db         数据库Handle
+ * @param         {char} *table         表名称
+ * @param         {char} *table_entry   表所包含的项
+ * @return        {int}
+ */
+int mysqlite_create_table(sqlite3 *db, char *table, char *table_entry);
+
+/**
+ * @description  : 向数据库的某个表中添加新的数据行
+ * @param         {sqlite3} *db         数据库Handle
+ * @param         {char} *table         表名称
+ * @param         {char} *val           要添加的新数据行
+ * @return        int
+ */
+int mysqlite_insert(sqlite3 *db, char *table, char *val);
+
+/**
+ * @description  : 删除数据库中已有的数据
+ * @param         {sqlite3} *db         数据库Handle
+ * @param         {char} *table         表名称
+ * @param         {char} *content       需要删除的数据
+ * @return        {*}
+ */
+int mysqlite_delete(sqlite3 *db, char *table, char *content);
+
+/**
+ * @description  : 删除数据库中已有的表
+ * @param         {sqlite3} *db         数据库Handle
+ * @param         {char} *table         表名称
+ * @return        {*}
+ */
+int mysqlite_delete_table(sqlite3 *db, char *table);
+
+/**
+ * @description  : 向数据库修改更新已有数据
+ * @param         {sqlite3} *db         数据库Handle
+ * @param         {char} *table         表名称
+ * @param         {char} *content       需要修改更新的内容
+ * @param         {int} sem_lock        信号量锁 1：开启 0：关闭
+ * @return        {*}
+ */
+int mysqlite_updata(sqlite3 *db, char *table, char *content, int sem_lock);
+
+/**
+ * @description  : 向数据库中读取已有的数据
+ * @param         {sqlite3} *db         数据库Handle
+ * @param         {char} *table         表名称
+ * @param         {char} *content       需要读取查询的内容
+ * @param         {char} *other         其它的sql语句内容
+ * @param         {query_callback_t} cb 查询回调函数
+ * @param         {void} *user_data     向回调函数传递的参数
+ * @return        {*}
+ */
+int mysqlite_query(sqlite3 *db, char *table, char *content, char *other, query_callback_t cb, void *user_data);
+
+#endif
